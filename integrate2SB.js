f8a15e63ec1e4bde914b23f8c9dc8e1ba304df70
MightyDevKnight <keithnelson0426@gmail.com>
2022-04-23 11:13:17 +0900

    update integrate2SB

--- integrate2SB.js
+++ integrate2SB.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const themeMetaDataBlob = fs.readFileSync("themes-storybook.json");
+const fs = require('fs');
+const themeMetaDataBlob = fs.readFileSync('dist/themes-storybook.json');
 const themeMetaData = JSON.parse(themeMetaDataBlob);
 
 const headerHTML = `
@@ -9,11 +9,8 @@ const headerHTML = `
         href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;600;700&family=IBM+Plex+Sans:wght@300;400;600;700&family=IBM+Plex+Serif:wght@300;400;600;700&display=swap"
         rel="stylesheet"
     />
-    ${themeMetaData.map(
-      (themeMeta) =>
-        `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MightyDevKnight/ft-plugin@latest/${themeMeta.path}" />`
-    )}
+    ${themeMetaData.map(themeMeta => `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MightyDevKnight/ft-plugin@latest/${themeMeta.path}" />`)}
 `;
 
-fs.writeFileSync('.storybook/manager-header.html', headerHTML, "utf-8");
-fs.writeFileSync('.storybook/preview-header.html', headerHTML, "utf-8");
+fs.writeFileSync('.storybook/manager-header.html', headerHTML, 'utf-8');
+fs.writeFileSync('.storybook/preview-header.html', headerHTML, 'utf-8');
