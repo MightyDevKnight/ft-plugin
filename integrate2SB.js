e2f6f8ddb166a1722406d098213324a4439e29a0
MightyDevKnight <keithnelson0426@gmail.com>
2022-04-23 11:43:48 +0900

    update integrate2SB and config

--- integrate2SB.js
+++ integrate2SB.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const themeMetaDataBlob = fs.readFileSync('dist/themes-storybook.json');
 const themeMetaData = JSON.parse(themeMetaDataBlob);
 
+const configBlob = fs.readFileSync('config.json');
+const config = JSON.parse(configBlob);
+
 const headerHTML = `
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -9,7 +12,7 @@ const headerHTML = `
         href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;600;700&family=IBM+Plex+Sans:wght@300;400;600;700&family=IBM+Plex+Serif:wght@300;400;600;700&display=swap"
         rel="stylesheet"
     />
-    ${themeMetaData.map(themeMeta => `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MightyDevKnight/ft-plugin@latest/${themeMeta.path}" />`)}
+    ${themeMetaData.map(themeMeta => `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/${config.owner}/${config.repo}@latest/${themeMeta.path}" />`)}
 `;
 
 fs.writeFileSync('.storybook/manager-header.html', headerHTML, 'utf-8');
